fce2e2d6f86ab0680cd30b489ed307f16d214c15
Adrian Machado <machadoadrian9705@gmail.com>
2021-06-28 18:15:58 -0300

    changes in code

--- App.js
+++ App.js
@@ -1,22 +1,48 @@
 import React from "react";
-import { Text, View, StyleSheet, Image, Button, Alert } from 'react-native'
+import { Text, View, StyleSheet, Image, Button } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import 'react-native-gesture-handler';
 
-const App = () => {
+
+const Stack = createStackNavigator()
+
+const HomeScreen = ({ navigation }) => {
   return (
-
     <View style={styles.container}>
-      <Text style={styles.title}>Hello World!!</Text>
+
+
+      <Text style={styles.title}>My First App</Text>
       <Image
         source={{ uri: 'https://picsum.photos/200/300' }}
         style={styles.image}
       />
-      <Button
-        color='red'
-        title='press me'
-        onPress={() => Alert.alert('Hello')}
-      />  
+
+      <Button title="Products"
+        onPress={() => navigation.navigate('ProductsScreen')}
+
+      />
 
     </View>
+  );
+}
+
+const ProductsScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen name='home' component={HomeScreen} />
+        <Stack.Screen name='Products' component={ProductsScreen} />
+      </Stack.Navigator>
+
+    </NavigationContainer>
+
   );
 
 };
@@ -28,9 +54,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: 'white'
   },
-  title: { fontSize: 30, color: 'black' },
-  image: { height: 150, width: 150, borderRadius: 90 }
-  
+  title: {
+    fontSize: 30,
+    color: 'black'
+  },
+  image: {
+    height: 150,
+    width: 150,
+    borderRadius: 90
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    marginTop: 10
+  },
+  buttonText: {
+    color: 'white'
+  }
+
 })
+
+
 
 export default App;
